27200ee6d4590a9189a505057f17f2fbae3cb09c
ELABBASSI Oussama <oussamaelabbasi@gmail.com>
2024-09-30 10:41:25 +0100

    cors adding

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 
 app.get('/',(req, res)=>{
   res.status(200).json({message : "hello ESP"});
