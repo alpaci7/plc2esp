342fa8004ee7e4446d589142e0373b4d0fde2857
ELABBASSI Oussama <41241807+alpaci7@users.noreply.github.com>
2024-09-30 10:43:24 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const cors = require("cors");
 
 const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 
 app.get('/',(req, res)=>{
   res.status(200).json({message : "hello ESP"});
